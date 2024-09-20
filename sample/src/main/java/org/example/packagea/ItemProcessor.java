b5680d025d37821c5481d800f3a3cd7033361565
Norman Walsh <ndw@nwalsh.com>
2024-09-20 08:30:46 +0100

    More tinkering with the sample for @link testing

---
+++ sample/src/main/java/org/example/packagea/ItemProcessor.java
@@ -0,0 +1,12 @@
+package org.example.packagea;
+
+import org.example.packageb.Item;
+
+/**
+ * This is a link to {@link Item}.
+ */
+public class ItemProcessor {
+    public ItemProcessor(Item item) {
+
+    }
+}
---
+++ sample/src/main/java/org/example/packageb/Item.java
@@ -0,0 +1,5 @@
+package org.example.packageb;
+
+public class Item {
+    // an item
+}
